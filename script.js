e68a71a4c30b048bc1245ba57d6772d7f2b339c4
Mantu Singh <mantusingh90@gmail.com>
2016-03-01 16:34:02 +0530

    Added files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+angular.module('myApp', []).
+   directive('barsChart', function ($parse) {
+     var directiveDefinitionObject = {
+         restrict: 'E',
+         replace: false,
+         scope: {data: '=chartData'},
+         link: function (scope, element, attrs) {
+           var chart = d3.select(element[0]);
+            chart.append("div").attr("class", "chart")
+             .selectAll('div')
+             .data(scope.data).enter().append("div")
+             .transition().ease("elastic")
+             .style("width", function(d) { return d + "%"; })
+             .text(function(d) { return d + "%"; });
+         } 
+      };
+      return directiveDefinitionObject;
+   });
+
+function Ctrl($scope) {
+    $scope.myData = [50,70,10,20,30,40,60,80,20];
+}
